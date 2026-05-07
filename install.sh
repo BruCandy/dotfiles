d1bef2825f51294267fd5e5698e5a8d07e91ff6e
brucandy-lab <brucandy-lab@users.noreply.github.com>
2026-05-08 07:34:33 +0900

    feat: update install.sh to add a failure handling

--- install.sh
+++ install.sh
@@ -5,7 +5,26 @@ DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 curl -f https://zed.dev/install.sh | sh
 
-mkdir -p ~/.config/zed
+mkdir -p "$HOME/.config/zed"
 
 cp "$DOTFILES_DIR/zed/settings.json" "$HOME/.config/zed/settings.json"
-cp "$DOTFILES_DIR/zed/keymap.json" "$HOME/.config/zed/settings.json"
+cp "$DOTFILES_DIR/zed/keymap.json" "$HOME/.config/zed/keymap.json"
+
+
+BASHRC="$HOME/.bashrc"
+
+if ! grep -q "show_last_status()" "$BASHRC"; then
+    cat >> "$BASHRC" <<'EOF'
+
+# Show previous command failure status
+show_last_status() {
+    local exit_code=$?
+
+    if [ "$exit_code" -ne 0 ]; then
+        echo -e "\e[31mfailed: exit $exit_code\e[0m"
+    fi
+}
+
+PROMPT_COMMAND=show_last_status
+EOF
+fi
